a95c7c124bd065db41d55025aefdb8ddf43ba2a1
Nate Finch <nate@thedigitalring.com>
2020-05-03 22:05:15 -0500

    add admin routes

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -26,10 +26,16 @@ const Routes = () => {
       />
       <Route path="/journals/{id}" page={JournalPage} name="journal" />
       <Route path="/journals" page={JournalsPage} name="journals" />
-      <Route path="/posts/new" page={NewPostPage} name="newPost" />
-      <Route path="/posts/{id:Int}/edit" page={EditPostPage} name="editPost" />
-      <Route path="/posts/{id:Int}" page={PostPage} name="post" />
-      <Route path="/posts" page={PostsPage} name="posts" />
+
+      <Route path="/admin/posts/new" page={NewPostPage} name="newPost" />
+      <Route
+        path="/admin/posts/{id:Int}/edit"
+        page={EditPostPage}
+        name="editPost"
+      />
+      <Route path="/admin/posts/{id:Int}" page={PostPage} name="post" />
+      <Route path="/admin/posts" page={PostsPage} name="posts" />
+
       <Route path="/about" page={AboutPage} name="about" />
       <Route path="/" page={HomePage} name="home" />
       <Route notfound page={NotFoundPage} />
